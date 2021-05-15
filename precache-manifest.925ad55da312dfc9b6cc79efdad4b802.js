2364229d38afadd64d83f283d021fb329e0306a8
Maksym Matuliak <maksymmatuliak@gmail.com>
2021-05-15 14:55:58 +0300

    Updates

---
+++ precache-manifest.925ad55da312dfc9b6cc79efdad4b802.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8895a3a62c7b7e23ee1cb01400b35f7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a843fbbb8af76093797a",
+    "url": "./static/css/main.51360e2c.chunk.css"
+  },
+  {
+    "revision": "78f236d8a74d3c2de0fa",
+    "url": "./static/js/2.f4524d81.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f4524d81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a843fbbb8af76093797a",
+    "url": "./static/js/main.d45db487.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  }
+]);
